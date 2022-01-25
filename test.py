d1f60bb73e17faafe86bb4a7f1bcc4e0417c3206
Bogdangarantov <47140987+Bogdangarantov@users.noreply.github.com>
2022-01-25 21:26:35 +0200

    test commit

---
+++ test.py
@@ -0,0 +1,5 @@
+print("And I wanna kiss you, make you feel alright")
+print("I'm just so tired to share my nights")
+print("I wanna cry and I wanna love")
+print("But all my tears have been used up")
+
