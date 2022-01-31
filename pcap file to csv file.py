2c35369b599e69a0ab4a77859570efa39ac8e777
Sotnikov Maksym <80080035@knu.ua>
2022-01-31 18:32:52 +0200

    initial setting

--- pcap file to csv file.py
+++ pcap file to csv file.py
@@ -3,7 +3,3 @@ from CSIKit.reader import get_reader
 my_reader = get_reader("D:/universe/samsung_practic/test_plate_standart/Falldown_1.pcap")
 csi_data = my_reader.read_file("D:/universe/samsung_practic/test_plate_standart/Falldown_1.pcap")
 
-from CSIKit.reader import IWLBeamformReader
-
-my_reader = IWLBeamformReader()
-csi_data = my_reader.read_file("path/to/log.all_csi.6.7.6.dat")
