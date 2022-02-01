fa06b740a11a5d33a96ee7ef31a89f39acf856f3
Bogdangarantov <47140987+Bogdangarantov@users.noreply.github.com>
2022-02-01 18:21:41 +0200

    changed_the_variable_in_for

--- csi_to_csv.py
+++ csi_to_csv.py
@@ -4,16 +4,16 @@ import pandas
 from CSIKit.reader import get_reader
 from CSIKit.util import csitools
 dir_pcap = os.listdir("pcap_files")
-for i in range( len(dir_pcap)):
-    files_list = os.listdir(f"pcap_files/{dir_pcap[i]}")
-    os.mkdir(f"csv_files/{dir_pcap[i]}")
+for i in dir_pcap:
+    files_list = os.listdir(f"pcap_files/{i}")
+    os.mkdir(f"csv_files/{i}")
     for j in files_list :
-        my_reader = get_reader(f"pcap_files/{dir_pcap[i]}/{j}")
-        csi_data = my_reader.read_file(f"pcap_files/{dir_pcap[i]}/{j}")
+        my_reader = get_reader(f"pcap_files/{i}/{j}")
+        csi_data = my_reader.read_file(f"pcap_files/{i}/{j}")
         csi_amplitude, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="amplitude")
         df = pandas.DataFrame(csi_amplitude.reshape(no_frames, no_subcarriers))
         new_filename = j.split('.')[0]+".csv"
-        df.to_csv(f"csv_files/{dir_pcap[i]}/{new_filename}.csv")
+        df.to_csv(f"csv_files/{i}/{new_filename}.csv")
 
 
 
