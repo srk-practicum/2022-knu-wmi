501472548b3fb5d593583ae13b538d3542955895
Sotnikov Maksym <80080035@knu.ua>
2022-02-01 19:45:42 +0200

    add csv

--- pcap file to csv file.py
+++ pcap file to csv file.py
@@ -1,5 +1,17 @@
 from CSIKit.reader import get_reader
+from CSIKit.util import csitools
+import pandas as pd
+import numpy as np
+import os
+dir_pcap = os.listdir(r"D:\universe\samsung_practic\test_plate_standart")
 
-my_reader = get_reader("D:/universe/samsung_practic/test_plate_standart/Falldown_1.pcap")
-csi_data = my_reader.read_file("D:/universe/samsung_practic/test_plate_standart/Falldown_1.pcap")
+for i in dir_pcap:
+    my_reader = get_reader(f"D:\\universe\\samsung_practic\\test_plate_standart\\{i}")
+    csi_data = my_reader.read_file(f"D:\\universe\\samsung_practic\\test_plate_standart\\{i}")
+    csi_phase, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="phase")
+    csi_amplitude, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="amplitude")
+    # df = pd.DataFrame(csi_amplitude.reshape(187, 256))
+    df = pd.DataFrame(csi_amplitude.reshape(no_frames, no_subcarriers))
+    new_filename = i.split('.')[0] + ".csv"
+    df.to_csv(path_or_buf=f"csv_files/{new_filename}.csv", header=False, index=False)
 
