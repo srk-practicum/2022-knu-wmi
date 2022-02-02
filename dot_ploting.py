47da133831eb6b1c820ab7d26802f3e24a2eb674
Bogdangarantov <47140987+Bogdangarantov@users.noreply.github.com>
2022-02-02 16:45:53 +0200

    Added chart and refactored csv_files dir

--- dot_ploting.py
+++ dot_ploting.py
@@ -2,21 +2,25 @@ import os
 import pandas
 import matplotlib.pyplot as plt
 
-
 dir_csv = os.listdir("csv_files")
-for i in dir_csv:
+colors = ['yellow', 'blue', 'orange', 'black', 'purple', 'brown', 'green']
+for ind, i in enumerate(dir_csv):
     files_list = os.listdir(f"csv_files/{i}")
     counter = 1
     for j in files_list:
         len_of_package = len(pandas.DataFrame(pandas.read_csv(f"csv_files/{i}/{j}")))
         counter += 1
-        plt.scatter(len_of_package, counter)
+        plt.scatter(len_of_package, counter, color=colors[ind])
+    plt.scatter(len_of_package, counter, color=colors[ind], label=i)
 
 plt.grid(True)
 plt.gca().set(xlim=(0, 200), ylim=(0, 50),
               xlabel='Packets', ylabel='Count')
-plt.xticks(fontsize=12); plt.yticks(fontsize=12)
-plt.legend(dir_csv, fontsize= 7)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.legend(fontsize=7)
+
+
 def save(name='', fmt='png'):
     pwd = os.getcwd()
     iPath = 'csv_files'.format(fmt)
@@ -26,4 +30,6 @@ def save(name='', fmt='png'):
     plt.savefig(f'plot_of_packages_and_count.png')
     os.chdir(pwd)
     plt.close()
+
+
 save(name='plot_of_packages_and_count', fmt='png')
