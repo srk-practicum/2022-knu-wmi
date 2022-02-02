7813aab29f092d5be16ac8394dd15de75d471024
Bogdangarantov <47140987+Bogdangarantov@users.noreply.github.com>
2022-02-02 18:47:45 +0200

    Replaced_all_and_added_IPYNB_NOTEBOOK

--- csi_to_csv.py
+++ csi_to_csv.py
@@ -4,18 +4,18 @@ from CSIKit.reader import get_reader
 from CSIKit.util import csitools
 
 # getting all dirs with pcap files
-dir_pcap = os.listdir("pcap_files")
+dir_pcap = os.listdir("pcap_files_one_move")
 # simple iteration of all dirs
 for i in dir_pcap:
     # getting all files in every dir
-    files_list = os.listdir(f"pcap_files/{i}")
+    files_list = os.listdir(f"pcap_files_one_move/{i}")
     # making new dir with same name in dir "csv_files"
     os.mkdir(f"csv_files/{i}")
     # simple iteration of all files in this dir
     for j in files_list:
         # reading our file
-        my_reader = get_reader(f"pcap_files/{i}/{j}")
-        csi_data = my_reader.read_file(f"pcap_files/{i}/{j}")
+        my_reader = get_reader(f"pcap_files_one_move/{i}/{j}")
+        csi_data = my_reader.read_file(f"pcap_files_one_move/{i}/{j}")
         # getting amplitude, no_frames and no_subcarriers
         csi_amplitude, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="amplitude")
         # making DataFrame
